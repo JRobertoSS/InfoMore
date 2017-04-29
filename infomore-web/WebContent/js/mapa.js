ffd8908cffa7afdc53683378f7f932a28588f23d
JRobertoSS <jrobertoss@outlook.com>
2017-04-29 16:28:19 -0300

    - Adiciona janela info para o meu local, com informações de raio e posição.
    - Corrige bugs relacionados ao meu local e atualizações com eventos de mudança de raio e posição

--- infomore-web/WebContent/js/mapa.js
+++ infomore-web/WebContent/js/mapa.js
@@ -5,19 +5,23 @@ $(document).ready(function() {
 	});
 });
 
-
-/*$("#buttonSaude").sideNav();
-$("#buttonEducacao").sideNav();
-$("#buttonSeguranca").sideNav();
-$("#buttonComodidades").sideNav();
-$("#buttonLazer").sideNav();
-$("#buttonTransporte").sideNav();
-$("#buttonOcorrencias").sideNav();*/
+/*
+ * $("#buttonSaude").sideNav(); $("#buttonEducacao").sideNav();
+ * $("#buttonSeguranca").sideNav(); $("#buttonComodidades").sideNav();
+ * $("#buttonLazer").sideNav(); $("#buttonTransporte").sideNav();
+ * $("#buttonOcorrencias").sideNav();
+ */
 
 document.mapIcones = new Map();
 
-document.mapIcones.set('Saúde', 'images/icon_saude.png').set('Educação', 'images/icon_educacao.png').set('Segurança', 'images/icon_seguranca.png').set('Comodidades', 'images/icon_comodidades.png').set('Lazer e Cultura', 'images/icon_lazer_cultura.png').set('Transportes', 'images/icon_transporte.png').set('Ocorrências', 'images/icon_ocorrencias.png').set('Meu Local', 'images/icon_person.png');
-
+document.mapIcones.set('Saúde', 'images/icon_saude.png').set('Educação',
+		'images/icon_educacao.png').set('Segurança',
+		'images/icon_seguranca.png').set('Comodidades',
+		'images/icon_comodidades.png').set('Lazer e Cultura',
+		'images/icon_lazer_cultura.png').set('Transportes',
+		'images/icon_transporte.png').set('Ocorrências',
+		'images/icon_ocorrencias.png').set('Meu Local',
+		'images/icon_person.png');
 
 // varre a lista de marcadores e seta o valor do map
 function setMapOnAll(map) {
@@ -28,7 +32,7 @@ function setMapOnAll(map) {
 	}
 }
 
-//Shows any markers currently in the array.
+// Shows any markers currently in the array.
 function showMarkers() {
 	setMapOnAll(document.map);
 }
@@ -44,8 +48,8 @@ function deleteMarkers() {
 	document.markers = [];
 }
 
-
-//função que cria um InfoWindow no evento de click no objetoParaOInfo, mostrando o conteudo
+// função que cria um InfoWindow no evento de click no objetoParaOInfo,
+// mostrando o conteudo
 function criarInfoWindow(conteudo, objetoParaOInfo) {
 	// criar um objeto de InfoWindow (descrição ao clicar no marcador)
 	var infoWindow = new google.maps.InfoWindow({
@@ -60,52 +64,77 @@ function criarInfoWindow(conteudo, objetoParaOInfo) {
 	})
 }
 
-function criarInfoWindowMeuLocal(marcador){
-	var conteudo = "<p>Este é seu local atual! </p><p></p><p>Info:</p>";
-	conteudo += "<p>Raio: " + document.circle.radius.toFixed(3) + "</p>";
-	conteudo += "<p>Latitude: " + document.meuLocal.lat() + "</p>";
-	conteudo += "<p>Longitude: " + document.meuLocal.lng() + "</p>";
-	criarInfoWindow(conteudo, marcador);
+function fechaInfoMeuLocal() {
+	if (document.infoWindow !== undefined && document.infoWindow !== null)
+		document.infoWindow.close();
+}
+function criarInfoWindowMeuLocal(marcador) {
+	fechaInfoMeuLocal();
+
+	var conteudo = "<p><b>Este é seu local atual!</b> </p><p><b>Info:</b></p>";
+	conteudo += "<p><b>Raio: </b>" + document.circle.radius.toFixed(3) + "</p>";
+	conteudo += "<p><b>Latitude: </b>" + document.meuLocal.lat() + "</p>";
+	conteudo += "<p><b>Longitude: </b>" + document.meuLocal.lng() + "</p>";
+
+	document.infoWindow = new google.maps.InfoWindow({
+		content : conteudo,
+		anchor : marcador.icon.anchor
+
+	})
+
+	google.maps.event.addListener(marcador, 'click', function() {
+		document.infoWindow.open(document.map, marcador);
+	})
 };
 
 // renderiza os pontos no objeto global de circle
 function renderizarPontos(pontos) {
-	// JAX-RS serializes an empty list as null, and a 'collection of one' as an object (not an 'array of one')
-	var list = pontos == null ? [] : (pontos instanceof Array ? pontos : [ pontos ]);
+	// JAX-RS serializes an empty list as null, and a 'collection of one' as an
+	// object (not an 'array of one')
+	var list = pontos == null ? [] : (pontos instanceof Array ? pontos
+			: [ pontos ]);
 
-	deleteMarkers(); // remove os marcadores anteriores e reinstancia o array de marcadores
+	deleteMarkers(); // remove os marcadores anteriores e reinstancia o array
+	// de marcadores
 	$.each(list, function(index, ponto) {
-		var localPonto = new google.maps.LatLng(ponto.latitude, ponto.longitude);
-		var image = {
-			url : document.mapIcones.get(ponto.categoria.nome),
-			anchor : new google.maps.Point(12, 12)
+		if (ponto != undefined && ponto.categoria != undefined) {
+			var localPonto = new google.maps.LatLng(ponto.latitude,
+					ponto.longitude);
+			var image = {
+				url : document.mapIcones.get(ponto.categoria.nome),
+				anchor : new google.maps.Point(12, 12)
+			}
+			var marker = new google.maps.Marker({
+				position : localPonto, // posição do ponto (LatLng)
+				map : document.map, // objeto do mapa
+				title : ponto.descricao, // título do marcador
+				icon : image
+			});
+			document.markers.push(marker); // adiciona o marker ao array
+			criarInfoWindow(ponto.descricao, marker); // cria um infoWindow
+														// para
+			// ao clicar no marcador,
+			// mostrar a descrição
 		}
-		var marker = new google.maps.Marker({
-			position : localPonto, // posição do ponto (LatLng)
-			map : document.map, // objeto do mapa
-			title : ponto.descricao, // título do marcador
-			icon : image
-		});
-		document.markers.push(marker); // adiciona o marker ao array
-		criarInfoWindow(ponto.descricao, marker); // cria um infoWindow para ao clicar no marcador, mostrar a descrição
 	});
 }
 
-
-//função que converte o objeto de bounds em JSON para envio de ajax
+// função que converte o objeto de bounds em JSON para envio de ajax
 function circleBoundsToJSON(bounds) {
 	return JSON.stringify({
 		"latNE" : bounds.getNorthEast().lat(), // latitude do ponto nordeste
 		"lngNE" : bounds.getNorthEast().lng(), // longitude do ponto nordeste
 		"latSW" : bounds.getSouthWest().lat(), // latitude do ponto sudoeste
-		"lngSW" : bounds.getSouthWest().lng() // longitude do ponto sudoeste
+		"lngSW" : bounds.getSouthWest().lng()
+	// longitude do ponto sudoeste
 	});
 }
 
-
-// função que realiza uma chamada ajax para o servidor, recuperando os pontos dentro de um determinado raio ( objeto google maps circle)
+// função que realiza uma chamada ajax para o servidor, recuperando os pontos
+// dentro de um determinado raio ( objeto google maps circle)
 function atualizarPontosRaio(circle) {
-	var bounds = circle.getBounds(); // objeto de LatLngBouunds https://developers.google.com/maps/documentation/javascript/reference#LatLngBounds
+	var bounds = circle.getBounds(); // objeto de LatLngBouunds
+	// https://developers.google.com/maps/documentation/javascript/reference#LatLngBounds
 
 	$.ajax({
 		type : 'POST',
@@ -113,19 +142,20 @@ function atualizarPontosRaio(circle) {
 		url : '/infomore/atualizaPontosRaio?acao=listar',
 		dataType : 'json',
 		data : circleBoundsToJSON(bounds), // função para converter em JSON
-		success : renderizarPontos // função para atualizar os pontos no raio
+		success : renderizarPontos
+	// função para atualizar os pontos no raio
 	});
 }
 
-//função que converte o objeto meuLocal em JSON para envio de ajax
-function meuLocalToJSON(meuLocal){
+// função que converte o objeto meuLocal em JSON para envio de ajax
+function meuLocalToJSON(meuLocal) {
 	return JSON.stringify({
 		"nomeLocal" : meuLocal.nomeLocal,
 		"raio" : meuLocal.raio,
 		"latitude" : meuLocal.latitude,
 		"longitude" : meuLocal.longitude,
-		
-		"limiteRaio" :{
+
+		"limiteRaio" : {
 			"pontoNE" : {
 				"latitude" : meuLocal.limiteRaio.pontoNE.latitude,
 				"longitude" : meuLocal.limiteRaio.pontoNE.longitude
@@ -136,62 +166,62 @@ function meuLocalToJSON(meuLocal){
 			}
 		}
 	});
-	
+
 }
 
-//função que retorna um novo objeto de meuLocal
-function getMeuLocalObject(){
+// função que retorna um novo objeto de meuLocal
+function getMeuLocalObject() {
 	var meuLocal = {
-			nomeLocal: "",
-		    raio: 0.0,
-		    latitude: 0.0,
-		    longitude: 0.0,
-		    limiteRaio: {
-		    	pontoNE:{
-		    		latitude: 0.0,
-		    		longitude: 0.0,
-		    	},
-		    	pontoSW: {
-					latitude: 0.0,
-		    		longitude: 0.0,
-		    	}
-		    }
-		}; 
+		nomeLocal : "",
+		raio : 0.0,
+		latitude : 0.0,
+		longitude : 0.0,
+		limiteRaio : {
+			pontoNE : {
+				latitude : 0.0,
+				longitude : 0.0,
+			},
+			pontoSW : {
+				latitude : 0.0,
+				longitude : 0.0,
+			}
+		}
+	};
 	return meuLocal;
 }
 
-//função que realiza uma chamada ajax para o servidor, salvando o local atual do usuário
+// função que realiza uma chamada ajax para o servidor, salvando o local atual
+// do usuário
 function salvarEsteLocal(nomeLocal) {
 	var meuLocal = getMeuLocalObject();
-	
-	meuLocal.nomeLocal = nomeLocal,
-	meuLocal.raio = document.circle.radius;
+
+	meuLocal.nomeLocal = nomeLocal, meuLocal.raio = document.circle.radius;
 	meuLocal.latitude = document.meuLocal.lat();
 	meuLocal.longitude = document.meuLocal.lng();
-	
-	meuLocal.limiteRaio.pontoNE.latitude =  document.circle.getBounds().getNorthEast().lat();
-	meuLocal.limiteRaio.pontoNE.longitude =  document.circle.getBounds().getNorthEast().lng();
-	meuLocal.limiteRaio.pontoSW.latitude =  document.circle.getBounds().getSouthWest().lat();
-	meuLocal.limiteRaio.pontoSW.longitude =  document.circle.getBounds().getSouthWest().lng();
-	
+
+	meuLocal.limiteRaio.pontoNE.latitude = document.circle.getBounds()
+			.getNorthEast().lat();
+	meuLocal.limiteRaio.pontoNE.longitude = document.circle.getBounds()
+			.getNorthEast().lng();
+	meuLocal.limiteRaio.pontoSW.latitude = document.circle.getBounds()
+			.getSouthWest().lat();
+	meuLocal.limiteRaio.pontoSW.longitude = document.circle.getBounds()
+			.getSouthWest().lng();
+
 	xhr = new XMLHttpRequest();
 	var url = '/infomore/detalhesMeuLocal';
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-type", "application/json");
 	var data = meuLocalToJSON(meuLocal);
 	xhr.send(data);
-	
-	/*$.ajax({
-		type : 'POST',
-		contentType : 'application/json',
-		url : '/infomore/detalhesMeuLocal',
-		dataType : 'json',
-		data : meuLocalToJSON(meuLocal), // função para converter em JSON
-		 success : renderizarPontos // função para atualizar os pontos no raio
-	});*/
+
+	/*
+	 * $.ajax({ type : 'POST', contentType : 'application/json', url :
+	 * '/infomore/detalhesMeuLocal', dataType : 'json', data :
+	 * meuLocalToJSON(meuLocal), // função para converter em JSON success :
+	 * renderizarPontos // função para atualizar os pontos no raio });
+	 */
 }
-
-
 
 function inicializaMapa(latitude, longitude) {
 
@@ -219,7 +249,7 @@ function inicializaMapa(latitude, longitude) {
 	};
 
 	// criar um novo objeto de marcador
-	var meuLocalmarker = new google.maps.Marker({
+	document.meuLocalmarker = new google.maps.Marker({
 		position : document.meuLocal, // posição no meuLocal (LatLng)
 		map : document.map, // objeto do mapa
 		title : 'Meu Local', // título do marcador
@@ -229,7 +259,8 @@ function inicializaMapa(latitude, longitude) {
 	document.circle = new google.maps.Circle({
 		map : document.map, // objeto do mapa
 		radius : 300, // raio inicial (em 'm')
-		center : meuLocalmarker.position, // centro do circulo no marcador
+		center : document.meuLocalmarker.position, // centro do circulo no
+		// marcador
 		strokeColor : '#1E90FF', // cor do contorno
 		strokeOpacity : 0.6, // opacidade do contorno
 		fillColor : '#1E90FF', // cor do preenchimento
@@ -251,38 +282,41 @@ function inicializaMapa(latitude, longitude) {
 	 */
 
 	// adicionar o listener do evento de mudança de raio
-	google.maps.event.addListener(document.circle, "radius_changed", function() {
+	google.maps.event.addListener(document.circle, "radius_changed",
+			function() {
 
-		/*
-		 * Aqui irá mandar uma requisição ajax, atualizando os
-		 * pontos dentro do raio
-		 * 
-		 */
-		atualizarPontosRaio(document.circle);
-		criarInfoWindowMeuLocal(meuLocalmarker);
+				/*
+				 * Aqui irá mandar uma requisição ajax, atualizando os pontos
+				 * dentro do raio
+				 * 
+				 */
+				atualizarPontosRaio(document.circle);
+				criarInfoWindowMeuLocal(document.meuLocalmarker);
 
-	})
+			})
 	// adicionar o listener do evento de mover o círculo
-	google.maps.event.addListener(document.circle, "center_changed", function() {
-		// mover o marcador para o novo local do círculo
-		meuLocalmarker.setPosition(document.circle.getCenter());
-		atualizarPontosRaio(document.circle);
-		criarInfoWindowMeuLocal(meuLocalmarker);
-	/*
-	 * document.getElementById("lat").innerHTML = "Latitude: " +
-	 * circle.getCenter().lat(); // mostra a latitude alterada no conteiner
-	 * lat document.getElementById("lng").innerHTML = "Longitude: " +
-	 * circle.getCenter().lng(); // mostra a longitude alterada no conteiner
-	 * lng
-	 */
-	})
+	google.maps.event.addListener(document.circle, "center_changed",
+			function() {
+				// mover o marcador para o novo local do círculo
+				document.meuLocal = document.circle.getCenter();
+				document.meuLocalmarker.setPosition(document.meuLocal);
+				atualizarPontosRaio(document.circle);
+				criarInfoWindowMeuLocal(document.meuLocalmarker);
+				/*
+				 * document.getElementById("lat").innerHTML = "Latitude: " +
+				 * circle.getCenter().lat(); // mostra a latitude alterada no
+				 * conteiner lat document.getElementById("lng").innerHTML =
+				 * "Longitude: " + circle.getCenter().lng(); // mostra a
+				 * longitude alterada no conteiner lng
+				 */
+			})
 
 	// criar infoWindow do marcador 'meu local'
-	criarInfoWindowMeuLocal(meuLocalmarker);
-	
+	criarInfoWindowMeuLocal(document.meuLocalmarker);
 
 	/**
-	 * Para testes - ao clicar no mapa (fora do circulo) printa no console de debug as coordenadas ( para exploração e inserts de pontos de teste)
+	 * Para testes - ao clicar no mapa (fora do circulo) printa no console de
+	 * debug as coordenadas ( para exploração e inserts de pontos de teste)
 	 */
 	google.maps.event.addListener(document.map, "click", function(event) {
 		var latLng = event.latLng;
@@ -291,14 +325,17 @@ function inicializaMapa(latitude, longitude) {
 
 }
 /**
- * Ao clicar em um botão de filtro de categoria, varre os marcadores e desativa/ativa todos estes marcadores pelo mapa
+ * Ao clicar em um botão de filtro de categoria, varre os marcadores e
+ * desativa/ativa todos estes marcadores pelo mapa
  */
 function filtrarCategorias(nomeCategoria) {
 	if (document.markers != undefined && document.markers != null) {
 		// varre os marcadores
 		for (var i = 0; i < document.markers.length; i++) {
-			// compara a url do icone com o mapa de url de icones (baseado no nome da categoria)
-			if (document.markers[i].icon.url == document.mapIcones.get(nomeCategoria)) {
+			// compara a url do icone com o mapa de url de icones (baseado no
+			// nome da categoria)
+			if (document.markers[i].icon.url == document.mapIcones
+					.get(nomeCategoria)) {
 				// se não está aparecendo no mapa, habilita o ícone
 				if (document.markers[i].getMap() == null)
 					document.markers[i].setMap(document.map);
@@ -314,8 +351,7 @@ function filtrarCategorias(nomeCategoria) {
 // atual
 function success(position) {
 	inicializaMapa(position.coords.latitude, position.coords.longitude);
-}
-;
+};
 
 function getLocal() {
 	// se o brower suportar, pega a geolocalizacao
@@ -323,13 +359,10 @@ function getLocal() {
 		navigator.geolocation.getCurrentPosition(success);
 	} else {
 		inicializaMapa(-23.507962, -46.188789); // posição da FATEC < em caso de
-	// erro ao recuperar posição
-	// atual
+		// erro ao recuperar posição
+		// atual
 	}
-}
-;
-
-
+};
 
 // executar o método depois de carregar a página
 /* window.onload = inicializaMapa(-23.505457, -46.187097); */
--- infomore-web/WebContent/view/mapa.jsp
+++ infomore-web/WebContent/view/mapa.jsp
@@ -3,7 +3,6 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta charset="UTF-8">
 
 <!-- Import da taglib pra uso de jstl -->
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
@@ -31,7 +30,7 @@
 
 
 <!--Let browser know website is optimized for mobile-->
-<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" charset="UTF-8"/>
 
 <title>Infomore</title>
 </head>
