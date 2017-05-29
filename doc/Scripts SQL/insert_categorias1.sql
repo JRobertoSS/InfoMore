588702842ace1972d6647c440db843f83fd81e6b
JRobertoSS <jrobertoss@outlook.com>
2017-05-29 19:29:39 -0300

    - Scripts do BD

---
+++ doc/Scripts SQL/insert_categorias1.sql
@@ -0,0 +1,17 @@
+use infomore;
+
+insert into categorias (id, nome, descricao) values (1, 'Saúde', 'Informe-nos quão importante é pra você a proximidade de farmácias, hospitais, postos de saúde e clínicas.');
+
+insert into categorias (id, nome, descricao) values (2, 'Educação', 'Informe-nos quão importante é pra você a proximidade de escolas, faculdades e creches.');
+
+insert into categorias (id, nome, descricao) values (3, 'Segurança', 'Informe-nos quão importante é pra você a proximidade de postos de bombeiros, polícia militar e civíl.');
+
+insert into categorias (id, nome, descricao) values (4, 'Comodidades', 'Informe-nos quão importante é pra você a proximidade de supermercados, padarias, restaurantes, lojas e postos de serviços.');
+
+insert into categorias (id, nome, descricao) values (5, 'Lazer e Cultura', 'Informe-nos quão importante é pra você a proximidade de cinemas, museus, teatros, bibliotecas, parques e bares.');
+
+insert into categorias (id, nome, descricao) values (6, 'Transportes', 'Informe-nos quão importante é pra você a proximidade de pontos de onibus, metrôs, trens e táxis.');
+
+insert into categorias (id, nome, descricao) values (7, 'Ocorrências', 'Informe-nos quão importante é pra você o baixo número de ocorrências de assaltos, homicídios, roubos de veículos, roubo de imóveis, acidentes de trânsitos e enchentes.');
+
+insert into categorias (id, nome, descricao) values (8, 'Meu Local', 'Este é o ponto central do meu local!');
